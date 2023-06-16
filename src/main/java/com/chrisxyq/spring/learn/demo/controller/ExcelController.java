2f71e864fc60320bd1a13a5a1a755a45c2d06d24
chrisxyq <yuanqixu@trip.com>
2023-06-16 17:44:51 +0800

    excel

--- src/main/java/com/chrisxyq/spring/learn/demo/controller/ExcelController.java
+++ src/main/java/com/chrisxyq/spring/learn/demo/controller/ExcelController.java
@@ -41,7 +41,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("student")
-public class StudentController {
+public class ExcelController {
     @Autowired
     private WebStudentListener webStudentListener;
 
